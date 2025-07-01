10a3ead6ab755b7113178d7245bb6a268ab2e526
YOOYONGHYUN <yyh901@naver.com>
2025-07-01 20:01:55 +0900

    🚚 orval api src/api 폴더 하위로 이동

--- orval.config.ts
+++ orval.config.ts
@@ -4,8 +4,8 @@ export default defineConfig({
   hopae: {
     input: { target: "https://studio-hopae.doslash.io/api-json" },
     output: {
-      target: "./src/queries/index.ts",
-      schemas: "./src/queries/model",
+      target: "./src/api/index.ts",
+      schemas: "./src/api/model",
       mock: false,
       override: {
         mutator: {
