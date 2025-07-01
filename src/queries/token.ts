be6f0a5a853db9a3e2f3ddfc6a8881b8d250d5e3
YOOYONGHYUN <yyh901@naver.com>
2025-07-01 20:02:29 +0900

    ✨ 토큰 가져오는 쿼리 생성

---
+++ src/queries/token.ts
@@ -0,0 +1,25 @@
+import { userControllerSignInWithClerk } from "@/api";
+import { getToken, saveToken } from "@/utils/token";
+import { useQuery } from "@tanstack/react-query";
+import { useAuth } from "@clerk/clerk-react";
+
+export const getPlayIdToken = () => {
+  const { isSignedIn } = useAuth();
+  return useQuery({
+    queryKey: ["playIdToken"],
+    queryFn: async () => {
+      try {
+        const token = await getToken();
+        if (!token) throw new Error("No token");
+        const res = await userControllerSignInWithClerk({ clerkToken: token });
+        if (!res.access_token) throw new Error("No access token");
+        saveToken(res.access_token);
+        return res;
+      } catch (err) {
+        throw err;
+      }
+    },
+    enabled: isSignedIn,
+    retry: false,
+  });
+};
