2c125a378c658b8ca6efb49927c2c8e2d4ab82a1
DongGukMon <2ast@2astui-MacBookPro.local>
2025-07-17 14:18:20 +0900

    feat: add generate roster button

--- src/pages/Bracket/index.tsx
+++ src/pages/Bracket/index.tsx
@@ -11,132 +11,141 @@ import type {GameType, Stage} from "@/api/model";
 import {createStage, deleteStage, getStages} from "@/queries/stage";
 import {useSelectedGameStore} from "@/stores/game";
 import CompetitionSelector from "@/components/CompetitionSelector";
+import {Button} from "@/components/ui/button";
 
 export type Sheet = {
-    id: string;
-    name: string;
+  id: string;
+  name: string;
 };
 
 export type Game = {
-    id: string;
-    name: string;
-    sheets: Sheet[];
+  id: string;
+  name: string;
+  sheets: Sheet[];
 };
 
 const Bracket = () => {
-    const {isExpand} = useExpandStore();
-    const {selectedGame, setSelectedGame} = useSelectedGameStore();
-    const {selectedCompetition} = useCompetition();
-    const {data: stageDatas} = getStages(
-        selectedCompetition?.id || 0,
-        selectedGame?.id || 0
-    );
-    const {mutate: createStageMutate} = createStage();
-    const {mutate: deleteStageMutate} = deleteStage();
-    const [stages, setStages] = useState<Stage[]>([]);
-    const games: GameType[] = selectedCompetition?.gameTypes || [];
+  const {isExpand} = useExpandStore();
+  const {selectedGame, setSelectedGame} = useSelectedGameStore();
+  const {selectedCompetition} = useCompetition();
+  const {data: stageDatas} = getStages(
+    selectedCompetition?.id || 0,
+    selectedGame?.id || 0
+  );
+  const {mutate: createStageMutate} = createStage();
+  const {mutate: deleteStageMutate} = deleteStage();
+  const [stages, setStages] = useState<Stage[]>([]);
+  const games: GameType[] = selectedCompetition?.gameTypes || [];
 
-    useEffect(() => {
-        if (selectedGame) return;
+  useEffect(() => {
+    if (selectedGame) return;
 
-        if (selectedCompetition && selectedCompetition.gameTypes[0]) {
-            setSelectedGame(selectedCompetition.gameTypes[0]);
-        }
-    }, [selectedCompetition]);
+    if (selectedCompetition && selectedCompetition.gameTypes[0]) {
+      setSelectedGame(selectedCompetition.gameTypes[0]);
+    }
+  }, [selectedCompetition]);
 
-    useEffect(() => {
-        if (!stageDatas) return;
+  useEffect(() => {
+    if (!stageDatas) return;
 
-        if (stageDatas?.length === 0) {
-            createStageMutate({
-                competitionId: selectedCompetition?.id || 0,
-                gameTypeId: selectedGame?.id || 0,
-                name: "스테이지 1",
-            });
-        } else {
-            setStages(stageDatas);
-        }
-    }, [stageDatas]);
+    if (stageDatas?.length === 0) {
+      createStageMutate({
+        competitionId: selectedCompetition?.id || 0,
+        gameTypeId: selectedGame?.id || 0,
+        name: "스테이지 1",
+      });
+    } else {
+      setStages(stageDatas);
+    }
+  }, [stageDatas]);
 
-    const handleAddStage = () => {
-        createStageMutate({
-            competitionId: selectedCompetition?.id || 0,
-            gameTypeId: selectedGame?.id || 0,
-            name: `스테이지 ${stages.length + 1}`,
-        });
-    };
+  const handleAddStage = () => {
+    createStageMutate({
+      competitionId: selectedCompetition?.id || 0,
+      gameTypeId: selectedGame?.id || 0,
+      name: `스테이지 ${stages.length + 1}`,
+    });
+  };
 
-    const handleDeleteStage = (id: number) => {
-        deleteStageMutate(id);
-    };
+  const handleDeleteStage = (id: number) => {
+    deleteStageMutate(id);
+  };
 
-    return (
-        <SidebarInset>
-            <header
-                className={`flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 ${
-                    isExpand ? "hidden" : "block"
-                }`}
+  return (
+    <SidebarInset>
+      <header
+        className={`flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 ${
+          isExpand ? "hidden" : "block"
+        }`}
+      >
+        <div className="flex items-center gap-2 px-4">
+          <SidebarTrigger className="-ml-1"/>
+          <Separator
+            orientation="vertical"
+            className="mr-2 data-[orientation=vertical]:h-4"
+          />
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem>
+                <BreadcrumbPage>
+                  <CompetitionSelector/>
+                </BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+        </div>
+      </header>
+      <div
+        className={`flex h-full flex-1 flex-col gap-4 p-4 ${
+          isExpand ? "pt-4" : "pt-0"
+        }`}
+      >
+        <Tabs
+          value={selectedGame?.id.toString() || ""}
+          className="h-full"
+          onValueChange={(value) => {
+            setSelectedGame(
+              games.find((game) => game.id.toString() === value) || games[0]
+            );
+          }}
+        >
+          <div className={`flex items-center gap-2 mb-6 ${isExpand ? "hidden" : "flex"}`}>
+            <TabsList
+              className="w-fit bg-zinc-800 rounded-md p-1"
             >
-                <div className="flex items-center gap-2 px-4">
-                    <SidebarTrigger className="-ml-1"/>
-                    <Separator
-                        orientation="vertical"
-                        className="mr-2 data-[orientation=vertical]:h-4"
-                    />
-                    <Breadcrumb>
-                        <BreadcrumbList>
-                            <BreadcrumbItem>
-                                <BreadcrumbPage>
-                                    <CompetitionSelector/>
-                                </BreadcrumbPage>
-                            </BreadcrumbItem>
-                        </BreadcrumbList>
-                    </Breadcrumb>
-                </div>
-            </header>
-            <div
-                className={`flex h-full flex-1 flex-col gap-4 p-4 ${
-                    isExpand ? "pt-4" : "pt-0"
-                }`}
-            >
-                <Tabs
-                    value={selectedGame?.id.toString() || ""}
-                    className="h-full"
-                    onValueChange={(value) => {
-                        setSelectedGame(
-                            games.find((game) => game.id.toString() === value) || games[0]
-                        );
-                    }}
+              {games.map((game) => (
+                <TabsTrigger
+                  className="cursor-pointer dark:data-[state=active]:bg-zinc-950"
+                  key={game.id}
+                  value={game.id.toString()}
                 >
-                    <TabsList
-                        className={`w-fit bg-zinc-800 rounded-md mb-6 p-1 ${
-                            isExpand ? "hidden" : "block"
-                        }`}
-                    >
-                        {games.map((game) => (
-                            <TabsTrigger
-                                className="cursor-pointer dark:data-[state=active]:bg-zinc-950"
-                                key={game.id}
-                                value={game.id.toString()}
-                            >
-                                {game.name}
-                            </TabsTrigger>
-                        ))}
-                    </TabsList>
-                    {selectedGame && stages.length > 0 && (
-                        <TabsContent value={selectedGame.id.toString()}>
-                            <BracketStage
-                                game={selectedGame}
-                                stages={stages}
-                                onAddStage={handleAddStage}
-                                onDeleteStage={handleDeleteStage}
-                            />
-                        </TabsContent>
-                    )}
-                </Tabs>
-            </div>
-        </SidebarInset>
-    );
+                  {game.name}
+                </TabsTrigger>
+              ))}
+            </TabsList>
+            <Button
+              variant="outline"
+              size="lg"
+              className="ml-auto"
+              disabled={false}
+            >
+              로스터 생성
+            </Button>
+          </div>
+          {selectedGame && stages.length > 0 && (
+            <TabsContent value={selectedGame.id.toString()}>
+              <BracketStage
+                game={selectedGame}
+                stages={stages}
+                onAddStage={handleAddStage}
+                onDeleteStage={handleDeleteStage}
+              />
+            </TabsContent>
+          )}
+        </Tabs>
+      </div>
+    </SidebarInset>
+  );
 };
 
 export default Bracket;
